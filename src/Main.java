3f977fd313ec1342a4f2dabd8378adefb9880f74
Yackov <yackov.irkyt@gmail.com>
2023-04-08 16:07:57 +0500

    Finished homework. Fixed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-
-        System.out.println("Ваш платеж в месяц составит: " + service.calculate(1000000,9.99,1));
-        System.out.println("Ваш платеж в месяц составит: " + service.calculate(1000000,9.99,2));
-        System.out.println("Ваш платеж в месяц составит: " + service.calculate(1000000,9.99,3));
+        System.out.println("Сумма кредита: 1000000 Р. Процентная ставка: 9.99 %. Срок: 1 год.");
+        System.out.println("Ваш платеж в месяц составит: " + service.calculate(1000000,9.99,1) + " Р");
+        System.out.println();
+        System.out.println("Сумма кредита: 1000000 р. Процентная ставка: 9.99 %. Срок: 2 года.");
+        System.out.println("Ваш платеж в месяц составит: " + service.calculate(1000000,9.99,2) + " Р");
+        System.out.println();
+        System.out.println("Сумма кредита: 1000000 р. Процентная ставка: 9.99 %. Срок: 3 года.");
+        System.out.println("Ваш платеж в месяц составит: " + service.calculate(1000000,9.99,3) + " Р");
     }
 }
 
