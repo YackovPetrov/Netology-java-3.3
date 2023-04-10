c0390eb081fb0659986d6667814fae3607c65c3e
Yackov <yackov.irkyt@gmail.com>
2023-04-10 21:39:01 +0500

    Corrected homework

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,13 +1,13 @@
 public class CreditPaymentService {
-    public double calculate (double a, double c, double b) {
-        double i = (c / 12) / 100; // кредитная ставка в месяц
-        double koef = (i * (Math.pow(1 + i, b * 12))) / (Math.pow(1 + i, b * 12) - 1); // коэффициент аннуитета
-        double result = a * koef; // итог
+    public double calculate (double Sum, double Percent, double CreditTerm) {
+        double InterestPerMonth = (Percent / 12) / 100; // кредитная ставка в месяц
+        double koef = (InterestPerMonth * (Math.pow(1 + InterestPerMonth, CreditTerm * 12))) / (Math.pow(1 + InterestPerMonth, CreditTerm * 12) - 1); // коэффициент аннуитета
+        double result = Sum * koef; // итог
         int pay = (int)result;
         return pay;
     }
 }
-//a сумма кредите
-//c процентная ставка
-//b срок кредита
-//i ставка в месяц
+//a сумма кредите - Sum
+//c процентная ставка - Percent
+//b срок кредита - CreditTerm
+//i ставка в месяц - InterestPerMonth
